841e75529f65c69078882e505dd9cc06db818638
Vostok <sergei.bubelov1@gmail.com>
2022-08-11 15:16:46 +1000

    add first usestate

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import s from './Post.module.css';
 
 const Post = (props) => {
+  let [count, setCount] = useState(0)
+  let increment = () => setCount(++count)
+  
   return (
     <div className={s.post}>
       <img src="https://pixelbox.ru/wp-content/uploads/2021/05/ava-vk-animal-91.jpg" alt="cat" />
       <span>{props.message}</span>
-      <span className={s.like}> {props.count} like</span>
+      <button onClick={increment} className={s.like}> {count} like</button>
     </div>
   )
 }
