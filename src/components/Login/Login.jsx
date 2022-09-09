88f3ad5100cae9af8e3bcc85cdde5cbe2d622df0
Vostokk <sergei.bubelov1@gmail.com>
2022-09-09 19:41:22 +1000

    add react-hook-form and create login-form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,8 +1,40 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+
+const LoginForm = props => {
+
+    const {
+        register,
+        handleSubmit,
+        // watch,
+        // formState: { errors }
+    } = useForm();
+
+    return (
+        <form onSubmit={handleSubmit(props.onSubmit)}>
+            <div>
+                <input {...register("Login")} type={"text"} placeholder={"login"}/>
+            </div>
+            <div>
+                <input {...register("Password")} type={"text"} placeholder={"password"}/>
+            </div>
+            <div>
+                <input {...register("checkbox")} type={"checkbox"}/> remember me
+            </div>
+            <div>
+                <button>Log in</button>
+            </div>
+        </form>
+    )
+};
 
 const Login = () => {
+    const onSubmit = data => console.log(data);
     return (
-        <h1>LOGIN</h1>
+        <div>
+            <h1>LOGIN</h1>
+            <LoginForm onSubmit={onSubmit}/>
+        </div>
     )
 };
 
