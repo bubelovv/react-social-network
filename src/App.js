e219557390e0b61d0b15b05fbcfe804646eeb9cc
Vostokk <sergei.bubelov1@gmail.com>
2022-09-28 15:07:31 +1000

    used lazy-components for routing

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, Suspense, lazy} from 'react'
 import './App.css'
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import Login from './components/Login/Login';
-import HeaderContainer from "./components/Header/HeaderContainer";
-import Navbar from './components/Navbar/Navbar'
-import ProfileContainer from "./components/Profile/ProfileContainer";
-import DialogsContainer from './components/Dialogs/DialogsContainer';
-import UsersContainer from "./components/Users/UsersContainer";
-import News from "./components/News/News";
-import Music from "./components/Music/Music";
-import Settings from "./components/Settings/Settings";
 import {connect, Provider} from "react-redux";
 import {initializeApp} from "./redux/appReducer";
-import Preloader from "./components/Users/Preloader/Preloader";
 import store from "./redux/reduxStore";
 
+// --------- my custom components ---------
+import Preloader from "./components/Users/Preloader/Preloader";
+import HeaderContainer from './components/Header/HeaderContainer';
+import Navbar from './components/Navbar/Navbar';
+// ----------------------------------------
+
+// ----------- lazy-components ------------
+const ProfileContainer = lazy(() => import('./components/Profile/ProfileContainer'));
+const DialogsContainer = lazy(() => import('./components/Dialogs/DialogsContainer'));
+const UsersContainer = lazy(() => import('./components/Users/UsersContainer'));
+const News = lazy(() => import('./components/News/News'));
+const Music = lazy(() => import('./components/Music/Music'));
+const Settings = lazy(() => import('./components/Settings/Settings'));
+const Login = lazy(() => import('./components/Login/Login'));
+// ----------------------------------------
+
 export function App(props) {
-const {initializeApp, initialized} = props;
+    const {initializeApp, initialized} = props;
 
     useEffect(() => {
         initializeApp()
@@ -30,16 +36,19 @@ const {initializeApp, initialized} = props;
                 <HeaderContainer/>
                 <Navbar/>
                 <div className="app-wrapper-content">
-                    <Routes>
-                        <Route path='/profile/:userId' element={<ProfileContainer/>}/>
-                        <Route path='/profile' element={<ProfileContainer/>}/>
-                        <Route path='/dialogs/*' element={<DialogsContainer/>}/>
-                        <Route path='/users' element={<UsersContainer/>}/>
-                        <Route path='/news' element={<News/>}/>
-                        <Route path='/music' element={<Music/>}/>
-                        <Route path='/settings' element={<Settings/>}/>
-                        <Route path='/login' element={<Login/>}/>
-                    </Routes>
+                    <Suspense fallback={<Preloader/>}>
+                        <Routes>
+                            <Route path='/profile/:userId' element={<ProfileContainer/>}/>
+                            <Route path='/profile' element={<ProfileContainer/>}/>
+                            <Route path='/' element={<ProfileContainer/>}/>
+                            <Route path='/dialogs/*' element={<DialogsContainer/>}/>
+                            <Route path='/users' element={<UsersContainer/>}/>
+                            <Route path='/news' element={<News/>}/>
+                            <Route path='/music' element={<Music/>}/>
+                            <Route path='/settings' element={<Settings/>}/>
+                            <Route path='/login' element={<Login/>}/>
+                        </Routes>
+                    </Suspense>
                 </div>
             </div>
     )
