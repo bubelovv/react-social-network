52ae34e867308de79fe359494223b939602fb23a
Vostokk <sergei.bubelov1@gmail.com>
2022-10-08 22:29:32 +1000

    typed thunks and reducer in profileReducer

--- src/redux/profileReducer.ts
+++ src/redux/profileReducer.ts
@@ -1,4 +1,6 @@
 import {profileApi} from '../API/api';
+import {ThunkAction} from "redux-thunk";
+import {RootState} from "./reduxStore";
 
 const ADD_POST = 'ADD-POST';
 const INCREMENT_LIKES = 'INCREMENT_LIKES';
@@ -9,16 +11,43 @@ const DELETE_POST = 'DELETE_POST';
 const SAVE_PHOTO_SUCCESS = 'SAVE_PHOTO_SUCCESS';
 
 interface AddPost {
-    type: typeof ADD_POST;
-    newPostText: string;
+    type: typeof ADD_POST,
+    newPostText: string
 }
 
-interface IncrementLikes {type: typeof INCREMENT_LIKES, userId: number}
-interface DecrementLikes {type: typeof DECREMENT_LIKES, userId: number}
-interface SetUserProfile {type: typeof SET_USER_PROFILE, profile: IProfile}
-interface SetStatus {type: typeof SET_STATUS, status: string}
-interface DeletePost {type: typeof DELETE_POST, id: number}
-interface SavePhotoSuccess {type: typeof SAVE_PHOTO_SUCCESS, photos: PhotosProfile}
+interface IncrementLikes {
+    type: typeof INCREMENT_LIKES,
+    userId: number
+}
+
+interface DecrementLikes {
+    type: typeof DECREMENT_LIKES,
+    userId: number
+}
+
+interface SetUserProfile {
+    type: typeof SET_USER_PROFILE,
+    profile: IProfile
+}
+
+interface SetStatus {
+    type: typeof SET_STATUS,
+    status: string
+}
+
+interface DeletePost {
+    type: typeof DELETE_POST,
+    id: number
+}
+
+interface SavePhotoSuccess {
+    type: typeof SAVE_PHOTO_SUCCESS,
+    photos: PhotosProfile
+}
+
+type ActionTypes = AddPost | IncrementLikes | DecrementLikes |
+    SetUserProfile | SetStatus | DeletePost | SavePhotoSuccess
+type ThunkType = ThunkAction<Promise<void>, RootState, undefined, ActionTypes>
 
 export const addPost = (newPostText: string): AddPost => ({type: ADD_POST, newPostText});
 export const incrementLikes = (userId: number): IncrementLikes => ({type: INCREMENT_LIKES, userId});
@@ -28,24 +57,26 @@ export const setStatus = (status: string): SetStatus => ({type: SET_STATUS, stat
 export const deletePost = (id: number): DeletePost => ({type: DELETE_POST, id});
 export const savePhotoSuccess = (photos: PhotosProfile): SavePhotoSuccess => ({type: SAVE_PHOTO_SUCCESS, photos});
 
-export const getProfile = (userId: number) => async (dispatch: any) => {
-    let data = await profileApi.getProfile(userId)
-    dispatch(setUserProfile(data))
+export const getProfile = (userId: number | null): ThunkType => async (dispatch) => {
+    if (userId !== null) {
+        let data = await profileApi.getProfile(userId)
+        dispatch(setUserProfile(data))
+    }
 }
 
-export const getStatus = (userId: number) => async (dispatch: any) => {
+export const getStatus = (userId: number): ThunkType => async (dispatch) => {
     let response = await profileApi.getStatus(userId)
     dispatch(setStatus(response.data))
 }
 
-export const updateStatus = (status: string) => async (dispatch: any) => {
+export const updateStatus = (status: string): ThunkType => async (dispatch) => {
     let response = await profileApi.updateStatus(status)
     if (response.data.resultCode === 0) {
         dispatch(setStatus(status));
     }
 }
 
-export const savePhoto = (file: any) => async (dispatch: any) => {      // file should be an object?
+export const savePhoto = (file: any): ThunkType => async (dispatch) => {      // file should be an object?
     let response = await profileApi.savePhoto(file)
     if (response.data.resultCode === 0) {
         await dispatch(savePhotoSuccess(response.data.data.photos));
@@ -61,24 +92,28 @@ export interface FormValues {
     contacts: ContactsProfile
 }
 
-export const saveInfo = (profile: FormValues, setError: any) => async (dispatch: any, getState: any): Promise<any> => {
-    let response = await profileApi.saveInfo(profile)
-    if (response.data.resultCode === 0) {
-        const userId = getState().auth.id
-        dispatch(getProfile(userId));
-    } else {
-        response.data.messages.forEach((message: string) => {
-            const name = message.slice(message.indexOf('>') + 1, message.indexOf(')'))
-            const mainName = name[0].toLowerCase() + name.slice(1)
-            setError(mainName, {type: 'server', message});
-        })
-        return Promise.reject()
+export const saveInfo = (profile: FormValues, setError: any): ThunkType => {
+    return async (dispatch, getState: () => RootState): Promise<any> => {
+        let response = await profileApi.saveInfo(profile)
+        if (response.data.resultCode === 0) {
+            const userId = getState().auth.id
+            await dispatch(getProfile(userId));
+        } else {
+            response.data.messages.forEach((message: string) => {
+                const name = message.slice(message.indexOf('>') + 1, message.indexOf(')'))
+                const mainName = name[0].toLowerCase() + name.slice(1)
+                setError(mainName, {type: 'server', message});
+            })
+            return Promise.reject()
+        }
     }
 }
 
-export interface IPost {id: number
+export interface IPost {
+    id: number
     message: string
-    likesCount: number}
+    likesCount: number
+}
 interface PhotosProfile {
     large: string | null
     small: string | null
@@ -118,7 +153,7 @@ const initialState: InitialStateProfile = {
     status: '',
 };
 
-const profileReducer = (state = initialState, action: any): InitialStateProfile => {
+const profileReducer = (state = initialState, action: ActionTypes): InitialStateProfile => {
     let posts = state.posts;
     switch (action.type) {
         case ADD_POST: {
@@ -163,12 +198,12 @@ const profileReducer = (state = initialState, action: any): InitialStateProfile 
         }
         case SAVE_PHOTO_SUCCESS: {
             return {
-                ...state, profile: {...state.profile, photos: action.photos},
-            } as InitialStateProfile;
+                ...state, profile: state.profile && {...state.profile, photos: action.photos},
+            }
         }
         default:
             return state;
     }
-};
+}
 
 export default profileReducer;
