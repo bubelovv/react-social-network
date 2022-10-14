ab86c8eaeb79f5366b0a4e4ef94f5da08cf21d2b
Vostokk <sergei.bubelov1@gmail.com>
2022-10-14 11:57:24 +1000

    replaced object-helper in utils folder and refactor it

---
+++ src/utils/object-helper.ts
@@ -0,0 +1,12 @@
+// Ищем в массиве array обьект, у которого по ключу itemKey лежит значение actionValue,
+// и в этом обьекте заменяем/добавляем свойство newObjProps
+import {IUser} from "../redux/usersReducer";
+
+export const objectsHelper = (array: IUser[], itemKey: string, actionValue: number, newObjProps: { followed: boolean }) => {
+    return array.map((item: IUser) => {
+        if (item[itemKey as keyof IUser] === actionValue) {
+            return {...item, ...newObjProps}
+        }
+        return item
+    })
+}
